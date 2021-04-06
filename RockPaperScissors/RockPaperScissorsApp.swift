a2c9c8ee38291c0c81dfc40dc02993cc3d4ef6b0
Pranav Kasetti <pranavkasetti@gmail.com>
2021-04-06 17:36:41 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Pranav Kasetti on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
